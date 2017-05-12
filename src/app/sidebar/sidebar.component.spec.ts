26c3efce96bd0cd01cbcf9183af6705d5f2b8556
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-12 11:55:58 +0200

    Fixed linting errors in sidebar.component.spec.ts

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
-import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
