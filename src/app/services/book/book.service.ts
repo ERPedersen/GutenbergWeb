ae5dba0b70710950b82043a83de4c7fe573c2463
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-15 02:32:32 +0200

    Finished BookService with RXJS BehaviorSubject

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -1,39 +1,24 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-
-import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable()
 export class BookService {
 
+  booksChanged$: BehaviorSubject<any> = new BehaviorSubject([]);
+
   constructor(private http: Http) {
   }
 
-  public getResponse(): Observable<object> {
-    return this.http.get('http://localhost:3000/books')
-      .map(this.extractData)
-      .catch(this.handleError);
+  public getBooks() {
+    return this.http.get('http://localhost:3000/books').
+      subscribe(res => this.booksChanged$.next(res.json()));
   }
 
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || { };
+  public getSubject(): Observable<any> {
+    return this.booksChanged$.asObservable();
   }
-
-  private handleError (error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
-
 }
