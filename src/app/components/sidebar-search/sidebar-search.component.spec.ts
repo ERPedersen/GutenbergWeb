22347748ba33cbb0bd70f09c4cbeea25d2920246
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-17 15:16:39 +0200

    Sidebar (#4)
    * Added sidebar with search form
    
    * Switched default value of 'type' to empty string in Search Form
    
    * Fixed linting errors in sidebar.component.spec.ts
    
    * Added unit tests to Search Form Component
    
    * Updated CSS to force 1200 px width
    
    * Added an http example service
    
    * Updated TestBed configurations
    
    * Restructured application
    
    * Fixed imports
    
    * Added font awesome icons
    
    * Updated mockdata
    
    * Added BookService as a global service
    
    * Finished BookService with RXJS BehaviorSubject
    
    * Added SidebarResults to sidebar
    
    * Added animations, delay and implementation of BookService in the search form
    
    * Added catching of RXJS subject in SidebarResultsComponent
    
    * added styling to results list
    
    * added styling to results
    
    * added json server script
    
    * updated unit tests and travis script
    
    * added tests again
    
    * adde json-server to travis
    
    * temporarily muted some tests which use http2
    
    * temporarily removed some tests
    
    * Tried fixing mock service
    
    * Tried fixing mock service
    
    * updated mockdata (#2)

--- src/app/components/sidebar-search/sidebar-search.component.spec.ts
+++ src/app/components/sidebar-search/sidebar-search.component.spec.ts
@@ -4,10 +4,38 @@ import {SidebarSearchComponent} from './sidebar-search.component';
 import {BookService} from '../../services/book/book.service';
 import {HttpModule, JsonpModule} from '@angular/http';
 import {FormsModule} from "@angular/forms";
+import {Observable} from "rxjs";
 
 describe('SidebarSearchComponent', () => {
   let component: SidebarSearchComponent;
   let fixture: ComponentFixture<SidebarSearchComponent>;
+  let bookService: BookService;
+
+  const SUBJECT = [
+    {
+      UID: 1643012885599,
+      title: "Flee To The Apocolypse",
+      text: 0,
+      authors: [
+        {
+          UID: 1633031650299,
+          name: "Sade J. Estrada"
+        }
+      ],
+      locations: [
+        {
+          name: "Crystal Springs",
+          lat: -66.18598,
+          long: 148.63678
+        },
+        {
+          name: "Blue Mountains",
+          lat: 35.64578,
+          long: 126.71533
+        }
+      ]
+    }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,13 +50,14 @@ describe('SidebarSearchComponent', () => {
         HttpModule,
         JsonpModule
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SidebarSearchComponent);
     component = fixture.componentInstance;
+    bookService = TestBed.get(BookService);
+    spyOn(bookService, 'getSubject').and.returnValue(Observable.of(SUBJECT));
     fixture.detectChanges();
   });
 
@@ -36,19 +65,19 @@ describe('SidebarSearchComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should invalidate the search form when type or query is empty', () => {
-  //   component.type = '';
-  //   component.query = '';
-  //   component.formSubmit();
-  //   expect(component.errors.type).toBeTruthy();
-  //   expect(component.errors.query).toBeTruthy();
-  // });
-  //
-  // it('should validate the search form when type and query is set', () => {
-  //   component.type = 'author';
-  //   component.query = 'test';
-  //   component.formSubmit();
-  //   expect(component.errors.type).toBeFalsy();
-  //   expect(component.errors.query).toBeFalsy();
-  // });
+  it('should invalidate the search form when type or query is empty', () => {
+    component.type = '';
+    component.query = '';
+    component.formSubmit();
+    expect(component.errors.type).toBeTruthy();
+    expect(component.errors.query).toBeTruthy();
+  });
+
+  it('should validate the search form when type and query is set', () => {
+    component.type = 'author';
+    component.query = 'test';
+    component.formSubmit();
+    expect(component.errors.type).toBeFalsy();
+    expect(component.errors.query).toBeFalsy();
+  });
 });
