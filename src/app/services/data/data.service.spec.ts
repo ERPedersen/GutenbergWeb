46cca2a2d7f57e0f79a4cabf042d226d594b0448
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-21 13:03:56 +0200

    Added method "getBooksFromAuthor" and unit tests (#7)

--- src/app/services/data/data.service.spec.ts
+++ src/app/services/data/data.service.spec.ts
@@ -1,15 +1,56 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { DataService } from './data.service';
+import {DataService} from './data.service';
+import {HttpModule, XHRBackend, ResponseOptions, Response} from "@angular/http";
+import {MockBackend, MockConnection} from "@angular/http/testing";
 
 describe('DataService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataService]
+      imports: [HttpModule],
+      providers: [
+        {provide: XHRBackend, useClass: MockBackend},
+        DataService
+      ]
     });
   });
 
   it('should ...', inject([DataService], (service: DataService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('HTTP Methods', () => {
+
+    beforeEach(inject([XHRBackend], (mockBackend) => {
+      mockBackend.connections.subscribe((connection: MockConnection) => {
+
+        let mockResponse: any = {type: "", data: [], search: false};
+
+        if (connection.request.url.indexOf("booksfromauthor") >= 0) {
+          mockResponse.type = "getBooksFromAuthor";
+          mockResponse.authorName = "Luke Skywalker";
+        }
+
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+    }));
+
+    it('should get the books from the api, and modify the response accordingly.', inject([DataService], (service: DataService) => {
+      let r;
+
+      service.getSubject().subscribe((results) => r = results);
+
+      service.getBooksFromAuthor("Luke Skywalker").subscribe((res) => {
+        expect(res.json().type).toBe("getBooksFromAuthor");
+        expect(res.json().authorName).toBe("Luke Skywalker");
+
+        expect(r.type).toBe("getBooksFromAuthor");
+        expect(r.authorName).toBe("Luke Skywalker");
+      });
+
+    }));
+  });
 });
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  resultsChanged$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+
+  constructor(private http: Http) {
+  }
+
+  public getBooksFromAuthor(authorName: string): any {
+
+    let uri = "http://localhost:8080/booksfromauthor?q=" + encodeURIComponent(authorName);
+
+    return this.http.get(uri)
+      .do((res) => {
+        let json = res.json();
+        json.authorName = authorName;
+        json.search = false;
+        json.type = "getBooksFromAuthor";
+        this.resultsChanged$.next(json)
+      })
+      .catch(() => {
+        let error = {authorName: authorName, data: [], error: true, search: false, type: "getBooksFromAuthor"};
+        this.resultsChanged$.next(error);
+        return Observable.throw(error);
+      });
+  }
+
+  public getSubject(): Observable<any> {
+    return this.resultsChanged$.asObservable();
+  }
 
 }
