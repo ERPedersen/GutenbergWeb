8ff75bcecb1b3a3b2c125c2caccf91fa7e5f5bc2
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-24 19:27:11 +0200

    Added markers to map when searching for book/author

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {DataService} from "../../services/data/data.service";
 
 @Component({
   moduleId: module.id,
@@ -8,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  public subscription: Subscription;
+  public lat: number = 29.745969;
+  public lng: number = 72.702225;
+  public markers = [];
+  public circle = {
+    lat: 0,
+    lng: 0,
+    visible: false
+  };
+  public zoom: number = 3;
+  public test = 0;
 
-  ngOnInit() {
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.dataService.getSubject().subscribe(results => this.addMapMarker(results));
+  }
+
+  public addMapMarker(results) {
+
+    if (results.type === "getBooksFromAuthor") {
+      let markers = [];
+      let books = results.data;
+
+      for (let b in books) {
+        let locations = results.data[b].locations;
+        for (let i in locations) {
+          markers.push({lat: locations[i].latitude, lng: locations[i].longitude})
+        }
+
+        this.markers = markers;
+        this.zoom = 3;
+      }
+    }
+  }
+
+  public mapClicked($event) {
+    console.log($event.coords.lat);
+    console.log($event.coords.lng);
+
+    this.circle.lat = $event.coords.lat;
+    this.circle.lng = $event.coords.lng;
+    this.circle.visible = true;
+
+    // Make lat long query
+
   }
 
 }
--- src/app/components/sidebar-results/sidebar-results.component.ts
+++ src/app/components/sidebar-results/sidebar-results.component.ts
@@ -7,8 +7,7 @@ import {DataService} from "../../services/data/data.service";
   moduleId: module.id,
   selector: 'app-sidebar-results',
   templateUrl: './sidebar-results.component.html',
-  styleUrls: ['./sidebar-results.component.scss'],
-  providers: [DataService]
+  styleUrls: ['./sidebar-results.component.scss']
 })
 export class SidebarResultsComponent implements OnInit, OnDestroy {
 
