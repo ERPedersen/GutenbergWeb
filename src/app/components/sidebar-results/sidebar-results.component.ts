cbd024e5ebc8869df50f7ba8c89a3f74b8db10b4
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-24 08:11:36 +0200

    added data-service to SidebarResults

--- src/app/components/sidebar-results/sidebar-results.component.ts
+++ src/app/components/sidebar-results/sidebar-results.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit, Input, OnDestroy} from '@angular/core';
 import {SearchService} from "../../services/search/search.service";
 import {Subscription} from 'rxjs';
+import {DataService} from "../../services/data/data.service";
 
 @Component({
   moduleId: module.id,
   selector: 'app-sidebar-results',
   templateUrl: './sidebar-results.component.html',
-  styleUrls: ['./sidebar-results.component.scss']
+  styleUrls: ['./sidebar-results.component.scss'],
+  providers: [DataService]
 })
 export class SidebarResultsComponent implements OnInit, OnDestroy {
 
@@ -14,7 +16,7 @@ export class SidebarResultsComponent implements OnInit, OnDestroy {
   public hasSearched;
   public subscription: Subscription;
 
-  constructor(private searchService: SearchService) {
+  constructor(private searchService: SearchService, private dataService: DataService) {
   }
 
   ngOnInit(): void {
