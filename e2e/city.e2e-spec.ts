6212dea6ba9b8c7dd7ef23bcde7d799c7d390f6d
NicolaiVBonderup <nvbonderup@gmail.com>
2017-05-25 17:01:46 +0200

    Added a file for City Protractor tests

---
+++ e2e/city.e2e-spec.ts
@@ -0,0 +1,14 @@
+import {browser, element, by, protractor} from 'protractor';
+import {GutenbergWebPage} from './app.po';
+
+describe('GutenbergWeb Cities', () => {
+  let page: GutenbergWebPage;
+
+  beforeEach(() => {
+    page = new GutenbergWebPage();
+  });
+
+
+
+
+});
